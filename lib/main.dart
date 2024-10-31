139190a3e01c2964883608dcfe105a563db2af41
AtalibAgAlmousleck <atalibletouareg@gmail.com>
2024-10-31 15:40:56 +0800

    started project with navbar

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodly/constants/constants.dart';
+import 'package:foodly/firebase_options.dart';
+import 'package:foodly/views/entrypoint.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+Widget defaultHome = MainScreen();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await GetStorage.init();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 825),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Foodly',
+          theme: ThemeData(
+            scaffoldBackgroundColor: kOffWhite,
+            iconTheme: const IconThemeData(color: kDark),
+            primaryColor: Colors.grey,
+          ),
+          home: defaultHome,
+        );
+      },
+    );
+  }
+}
