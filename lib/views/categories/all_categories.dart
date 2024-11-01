f8b7bacf066a5ae5bf226ad6ef8273ab0fa5fc6b
AtalibAgAlmousleck <atalibletouareg@gmail.com>
2024-11-01 09:36:59 +0800

    all category & show category pages

--- lib/views/categories/all_categories.dart
+++ lib/views/categories/all_categories.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:foodly/common/app_style.dart';
+import 'package:foodly/common/back_ground_container.dart';
+import 'package:foodly/common/reusable_text.dart';
+import 'package:foodly/constants/constants.dart';
+import 'package:foodly/constants/uidata.dart';
+import 'package:foodly/views/categories/widgets/category_tile.dart';
 
 class AllCategories extends StatelessWidget {
   const AllCategories({super.key});
@@ -7,7 +14,26 @@ class AllCategories extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('All Categories'),
+        elevation: 0,
+        backgroundColor: kOffWhite,
+        title: ReusableText(
+          text: 'Categories',
+          style: appStyle(17, kGray, FontWeight.w600),
+        ),
+      ),
+      body: BackGroundContainer(
+        color: Colors.white,
+        child: Container(
+          padding: EdgeInsets.only(left: 12.w, top: 10.h),
+          height: height,
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: List.generate(categories.length, (index) {
+              var category = categories[index];
+              return CategoryTile(category: category);
+            }),
+          ),
+        ),
       ),
     );
   }
