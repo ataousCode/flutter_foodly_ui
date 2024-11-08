767259e0466d7ed66f603d3b3f2bec4d33c7da11
AtalibAgAlmousleck <atalibletouareg@gmail.com>
2024-11-08 18:01:05 +0800

    fetch food hook

---
+++ lib/hooks/fetch_all_foods.dart
@@ -0,0 +1,50 @@
+import 'package:foodly/constants/constants.dart';
+import 'package:foodly/models/api_error.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:foodly/models/foods_model.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/hook_models/hook_result.dart';
+
+FetchHook useFecthAllFoods(String code) {
+  final foods = useState<List<Food>?>(null);
+  final isLoading = useState<bool>(false);
+  final error = useState<Exception?>(null);
+  final apiError = useState<ApiError?>(null);
+
+  Future<void> fetchData() async {
+    isLoading.value = true;
+
+    try {
+      Uri url = Uri.parse('$appBaseUrl/api/v1/foods/recommendation/$code');
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        foods.value = foodFromJson(response.body);
+      } else {
+        apiError.value = apiErrorFromJson(response.body);
+      }
+    } catch (ex) {
+      //error.value = Exception(ex.toString());
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  useEffect(() {
+    fetchData();
+    return null;
+  }, []);
+
+  void refetch() {
+    isLoading.value = true;
+    fetchData();
+  }
+
+  return FetchHook(
+    data: foods.value,
+    isLoading: isLoading.value,
+    error: error.value,
+    refetch: refetch,
+  );
+}
--- lib/views/home/widgets/food_list.dart
+++ lib/views/home/widgets/food_list.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:foodly/constants/uidata.dart';
+import 'package:foodly/common/shimmers/nearby_shimmer.dart';
+import 'package:foodly/hooks/fetch_foods.dart';
+import 'package:foodly/models/foods_model.dart';
 import 'package:foodly/views/home/widgets/food_widget.dart';
 
-class FoodList extends StatelessWidget {
+class FoodList extends HookWidget {
   const FoodList({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final hookResults = useFecthFoods('41007428');
+    List<Food>? foods = hookResults.data;
+    final isLoanding = hookResults.isLoading;
     return Container(
       height: 184.h,
       padding: EdgeInsets.only(left: 12.w, top: 10.h),
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: List.generate(foods.length, (index) {
-          var food = foods[index];
-          return FoodWidget(
-            image: food['imageUrl'],
-            title: food['title'],
-            time: food['time'],
-            price: food['price'].toStringAsFixed(2),
-          );
-        }),
-      ),
+      child: isLoanding
+          ? const NearbyShimmer()
+          : ListView(
+              scrollDirection: Axis.horizontal,
+              children: List.generate(foods!.length, (index) {
+                Food food = foods[index];
+                return FoodWidget(
+                  image: food.imageUrl[0],
+                  title: food.title,
+                  time: food.time,
+                  price: food.price.toStringAsFixed(2),
+                );
+              }),
+            ),
     );
   }
 }
